7164ee337fa6d20d0585727febb23fa12f305df0
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-08-11 12:21:42 +0200

    first commit

---
+++ database/migrations/9999_08_11_101116_add_foreign_keys.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+
+            $table->foreignId('type_id')->constrained();
+        });
+
+        Schema::table('project_technology', function (Blueprint $table) {
+
+            $table->foreignId('project_id')->constrained();
+            $table->foreignId('technology_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+
+            $table->dropForeign('projects_type_id_foreign');
+
+            $table->dropColumn('type_id');
+        });
+
+        Schema::table('project_technology', function (Blueprint $table) {
+
+            $table->dropForeign('project_technology_project_id_foreign');
+            $table->dropForeign('project_technology_technology_id_foreign');
+
+            $table->dropColumn('project_id');
+            $table->dropColumn('technology_id');
+        });
+    }
+};
