69a4af456570373a5d3df16a215e688dfe56a4f7
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-11-30 18:11:36 +0100

    test link logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,14 +70,15 @@
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                         <a class="dropdown-item" href="{{ url('admin/dashboard') }}">{{ __('Dashboard') }}</a>
                                         <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                        {{-- <a class="dropdown-item" href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                             {{ __('Logout') }}
-                                        </a>
+                                        </a> --}}
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="">
                                             @csrf
+                                            <button type="submit">Logout</button>
                                         </form>
                                     </div>
                                 </li>
