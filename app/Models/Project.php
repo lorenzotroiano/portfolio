a7930439fd3c1763796d82c2ae5c85fde3a07edb
lorenzotroiano <lorenzo.troiano1999@gmail.com>
2023-11-30 19:00:50 +0100

    fucntion img

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -18,6 +19,13 @@ class Project extends Model
 
         'type_id'
     ];
+
+    protected function immagine(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string|null $value) => $value ? asset('storage/' . $value) : null,
+        );
+    }
 
     public function type()
     {
